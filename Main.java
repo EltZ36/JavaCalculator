7ec01e61513a77c24e500f63f75e01ef315ecef3
Elton <32944003+EltZ36@users.noreply.github.com>
2021-08-15 20:54:29 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
    #options are adding/substracting/mulitplication/division 
+    Scanner question = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter the options that you want: Addition (1) Substraction (2) Multiplication (3) Division (4)");
+    String answer = question.nextLine();  
   }
 }

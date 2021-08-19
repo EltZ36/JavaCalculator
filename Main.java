58eaeaa49d15159c2e747166e921f5ba89400177
Elton <32944003+EltZ36@users.noreply.github.com>
2021-08-19 10:13:57 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
-class Main {
+public class Main {
   public static void main(String[] args) {
-   #options are adding/substracting/mulitplication/division 
+   //options are adding/substracting/mulitplication/division 
     Scanner question = new Scanner(System.in);  // Create a Scanner object
     System.out.println("Enter the options that you want: Addition (1) Substraction (2) Multiplication (3) Division (4)");
     String answer = question.nextLine();  
+    //add options and numbers to add. maybe make it so that you can either add more or stop after a certain point. 
+    if (answer == 1){
+    }
   }
 }
